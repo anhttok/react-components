932de181ac6a3b4c97a5c25645cabc6fcae08261
Anh Tran <anhttok@gmail.com>
2022-06-10 16:26:52 +0700

    Add form login

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './components/login-form/login-form-1';
+import './styles.scss';
+function App() {
+  return (
+    <div className="app">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
