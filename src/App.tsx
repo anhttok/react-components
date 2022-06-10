6d2087cd1f77de60e848e67fcc5149c35ac61b57
Anh Tran <anhttok@gmail.com>
2022-06-10 16:41:07 +0700

    Add local font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import LoginForm from './components/login-form/login-form-1';
+import './fonts/Poppins-Bold.ttf';
+import './fonts/Poppins-Regular.ttf';
 import './styles.scss';
+
 function App() {
   return (
     <div className="app">
